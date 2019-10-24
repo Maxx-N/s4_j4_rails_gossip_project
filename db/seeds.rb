d73c9ee5bce3a31ded856f6f6458510cf89fcda0
Maxx-N <maximenorval@gmail.com>
2019-10-24 23:18:16 +0200

    Chaque utilisateur habite dans une ville et elle peut être modifiée

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@
 
 # On affiche la liste des utilisateurs (en limitant le nombre de colonnes) :
   puts "\n"
-  puts "10 utilisateurs ont été ajoutés :"
+  puts "10 utilisateurs ont été ajouté(e)s :"
   puts "\n"
   tp User.all, :first_name, :last_name, :email, :age
 
@@ -32,18 +32,30 @@
     c.save
   end
 
-# On affiche la liste des villes (en limitant le nombre de colonnes)
+# On affiche la liste des villes (en limitant le nombre de colonnes) :
   puts "\n"
   puts "10 villes ont été ajoutées :"
   puts "\n"
   tp City.all, :id, :name, :zip_code
 
-
-=begin
-  i = 0
-  while i<10
-    User.all[i].city = City.find(i+1) 
-    User.all[i].save
-    i += 1
+# On attribue aléatoirement une ville à un  utilisateur (plusieurs utilisateurs peuvent venir de la même ville) :
+  User.all.each do |u|
+    u.city = City.all[rand(0..9)]
+    u.save
   end
-=end
+
+# On affiche chaque utilisateur et la ville d'où il vient :
+  puts "\n"
+  puts "Voici les villes des utilisateurs(trices) :"
+  puts "\n"
+  User.all.each do |u|
+    puts "- #{u.first_name} #{u.last_name.upcase} habite à #{u.city.name}"
+  end
+
+# On change aléatoirement la ville de l'utilisateur ayant l'ID n°3 et on l'affiche 
+  puts "\n"
+  puts "#{User.find(3).first_name} vient de déménager..."
+  puts "\n"
+  print "Il(elle) n'habite plus à #{User.find(3).city.name}"
+  User.find(3).update(city_id: rand(1..10))
+  puts " mais à #{User.find(3).city.name} !"
